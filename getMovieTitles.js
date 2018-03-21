e78efd914d6eb6c8adea3f4cdc1192c5abc977f0
github username <github email address>
2018-03-21 19:32:40 -0400

    done

--- getMovieTitles.js
+++ getMovieTitles.js
@@ -1,26 +1,28 @@
 function getMovieTitles(substr) {
     let currentPage = 1
-    let URL = "https://jsonmock.hackerrank.com/api/movies/search/?Title="+substr+"&page="
+    let URL = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=`
     let urls = []
     let totalPages = 0
-
+    
     fetch(URL+currentPage)
     .then(res=>res.json())
     .then(function(data) {
         totalPages = data.total_pages
-        for (let i=1 ; i<=totalPages; i++ ){
+        for (let i=1; i<=totalPages; i++ ){
             urls.push(URL+i)
         }
+        
+        Promise.all(urls.map(x=>fetch(x)
+          .then(res=>res.json()) ))
+          .then(resp=>{
+            let data = resp.map(resp=>resp.data)
 
-        Promise.all(urls.map(url=>fetch(url)
-          .then(resp => resp.json()) ))
-          .then(resp => {
-            let data = resp.map(resp => resp.data )
-            let movieTitles = [].concat.apply([],data).map(movie => movie.Title)
-            movieTitles.sort()
-            console.log(movieTitles)
+            let titelsArr = []
+            data.forEach(x=>{ x.forEach(x=>titelsArr.push(x.Title)) })
+            
+            titelsArr.sort()
+            console.log(titelsArr)
         })
     })
 }
-
 getMovieTitles("spiderman")
